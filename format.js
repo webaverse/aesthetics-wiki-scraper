b657fcc6bedf9337ddf51428d1b23b589bb488bf
Avaer Kazmer <a@modules.io>
2022-09-06 14:51:02 -0400

    Add format.js

---
+++ format.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const fs = require('fs');
+// const {URL} = require('url');
+const {traverse, parsePage, getPageUrls, getPageName} = require('./util.js');
+
+const mkdirp = require('mkdirp');
+
+//
+
+const formattedDataDirectory = 'formatted-data';
+
+//
+
+const _makePageCache = () => ({
+  map: new Map(),
+  add(name, trope) {
+    this.map.set(name, trope);
+  },
+  toJSON() {
+    return Object.fromEntries(this.map);
+  },
+});
+const aestheticsCache = _makePageCache();
+const childrenCache = {
+  map: new Map(),
+  add(parentName, childName) {
+    let children = this.map.get(parentName);
+    if (children === undefined) {
+      children = [];
+      this.map.set(parentName, children);
+    }
+    children.push(childName);
+  },
+  toJSON() {
+    return Object.fromEntries(this.map);
+  },
+};
+
+//
+
+(async () => {
+  mkdirp.sync(formattedDataDirectory);
+
+  let i = 0;
+  const _log = () => {
+    console.log(JSON.stringify(aestheticsCache, null, 2));
+    console.log(JSON.stringify(childrenCache, null, 2));
+  };
+  const logRate = 100;
+  const _tryLog = () => {
+    if ((++i) % logRate === 0) {
+      _log();
+    }
+  };
+
+  await traverse((url, depth, $) => {
+    if (depth > 0) {
+      console.log(url);
+
+      const page = parsePage($);
+      const name = getPageName(url);
+
+      aestheticsCache.add(name, page);
+
+      const childUrls = getPageUrls($);
+      for (const childUrl of childUrls) {
+        const childName = getPageName(childUrl);
+        childrenCache.add(name, childName);
+      }
+      
+      // console.log(page);
+
+      // _tryLog();
+    }
+  }, {
+    download: false,
+  });
+
+  // write formatted data
+  console.log('write 1');
+  fs.writeFileSync(path.join(formattedDataDirectory, 'aesthetics.json'), JSON.stringify(aestheticsCache), 'utf8');
+  console.log('write 2');
+  fs.writeFileSync(path.join(formattedDataDirectory, 'children.json'), JSON.stringify(childrenCache), 'utf8');
+  console.log('write done');
+})();
